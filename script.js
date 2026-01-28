f9d673b95db5144dabfad16e51822695fbb80cfb
kallutlasaitharunnov17 <kallutlasaitharun@eageit.com>
2026-01-28 17:48:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-document.getElementById("log").innerText = "Log: Page loaded";
+let localStream;
+let peerConnection;
+
+const startBtn = document.getElementById("startBtn");
+const endBtn = document.getElementById("endBtn");
+const localSDP = document.getElementById("localSDP");
+const remoteSDP = document.getElementById("remoteSDP");
+const setRemoteBtn = document.getElementById("setRemoteBtn");
+const remoteAudio = document.getElementById("remoteAudio");
+
+const servers = { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] };
+
+startBtn.onclick = async () => {
+  startBtn.disabled = true;
+  endBtn.disabled = false;
+
+  // Get microphone
+  localStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  
+  // Create PeerConnection
+  peerConnection = new RTCPeerConnection(servers);
+
+  // Add local audio track
+  localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+
+  // Set remote audio
+  peerConnection.ontrack = event => {
+    remoteAudio.srcObject = event.streams[0];
+  };
+
+  // Gather ICE candidates
+  peerConnection.onicecandidate = event => {
+    if (event.candidate === null) {
+      localSDP.value = JSON.stringify(peerConnection.localDescription);
+    }
+  };
+
+  // Create offer
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+};
+
+setRemoteBtn.onclick = async () => {
+  const remoteDescription = JSON.parse(remoteSDP.value);
+  await peerConnection.setRemoteDescription(remoteDescription);
+
+  if (remoteDescription.type === "offer") {
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+    localSDP.value = JSON.stringify(peerConnection.localDescription);
+  }
+};
+
+endBtn.onclick = () => {
+  peerConnection.close();
+  peerConnection = null;
+  startBtn.disabled = false;
+  endBtn.disabled = true;
+  localSDP.value = "";
+  remoteSDP.value = "";
+  remoteAudio.srcObject = null;
+};
